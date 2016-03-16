64343610efd2dd3880715933122186f7176e9a42
HenryRL <1300427@rgu.ac.uk>
2016-03-16 15:23:34 +0000

    Added files via upload

--- login.php
+++ login.php
@@ -55,24 +55,6 @@ session_destroy()
 <!--design has been followed from Simpletut.com via https://www.youtube.com/watch?v=Qqcj4nYkcks'-->
 <html>
 <head>
-    <?php
-            include("connect.php"); //Establishing connection to our database
-        if (empty($_POST ["username"]) || empty($_POST ["password"])) {
-    echo "Both fields are required.";
-} else {
-    $username = $_POST ['username'];
-    $password = $_POST ['password'];
-    echo $username;
-    echo $password;
-    $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
-    $result=mysqli_query($db,$sql);
-    if (mysqli_num_rows($result) == 1) {
-        header("location: FileUpload.php"); // Redirecting to another page
-    } else {
-        echo "Incorrect username or password";
-    }
-}
-    ?>
     <meta charset="utf-8">
     <title>Ginger Bugginess</title>
     <link rel="stylesheet" href="layout.css" type="text/css" />
