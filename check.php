8e117323eaaa74f4456156123f6a75ddd8e8040e
HenryRL <1300427@rgu.ac.uk>
2016-03-18 18:48:05 +0000

    Added files via upload

--- check.php
+++ check.php
@@ -1,5 +1,4 @@
 <?php
-include('connect.php');
 session_start();
 $user_check=$_SESSION['username'];
 
@@ -13,4 +12,5 @@ if(!isset($user_check))
 {
 header("Location: home.php");
 }
+session_destroy()
 ?>
