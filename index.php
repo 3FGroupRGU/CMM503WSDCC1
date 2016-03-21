0d4231cd37fbb0aefb0ee72247a79984eed32ccc
HenryRL <1300427@rgu.ac.uk>
2016-03-21 15:01:57 +0000

    Added files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<!doctype html>
+<!--design has been followed from Simpletut.com via https://www.youtube.com/watch?v=Qqcj4nYkcks'-->
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Ginger Bugginess</title>
+    <link rel="stylesheet" href="layout.css" type="text/css" />
+    <link rel="stylesheet" href="menu.css" type="text/css" />
+</head>
+
+<body>
+<div id="holder">
+<div id="header">
+    <h1>Ginger Bugginess Fault Tracker</h1>
+</div>
+<div id="NavBar">
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">Forgot Login</a></li>
+        </ul>
+    </nav>
+</div>
+<div id="content">
+    <div id="pageheading">
+        <h1>Welcome</h1>
+    </div>
+    <div id="contentleft">
+        <h2>Welcome to the Ginger Bugginess fault logging page.</h2><br>
+        <h3>Site Down For Maintenance</h3><br>
+        <p>This site is temporarily unavailable.</p><br>
+        <p>This is due to the numpty who's working on it being a pillock and having to re-wrie all his PHP.</p>
+    </div>
+    <div id="contentright">
+        <h2>Nothing Here to See!</h2><br>
+        <p>Please be good little sheep and move along.....</p>
+            <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
+    </div>
+</div>
+<div id="footer"></div>
+</body>
+</html>
+
