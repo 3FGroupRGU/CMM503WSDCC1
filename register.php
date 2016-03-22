0947c0976d32559f9b11a3bef7eee93e34a2aad1
HenryRL <1300427@rgu.ac.uk>
2016-03-22 16:26:58 +0000

    Added files via upload

--- register.php
+++ register.php
@@ -18,7 +18,7 @@ session_destroy()
 </head>
 
 <body>
-<div id="holder"></div>
+<div id="holder">
 <div id="header">
     <h1>Ginger Bugginess Fault Tracker</h1>
 </div>
@@ -61,6 +61,7 @@ session_destroy()
 
         </div>
     </div>
+</div>
 </div>
 <div id="footer"></div>
 </body>
