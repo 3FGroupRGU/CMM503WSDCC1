8ca0be70411d96086ce3158ff044bf28fedf5d02
HenryRL <1300427@rgu.ac.uk>
2016-03-22 10:51:08 +0000

    Added files via upload

--- bug.php
+++ bug.php
@@ -10,15 +10,6 @@ $bugDesc=$row['desc'];
 echo"<h2>".$bugTitle."</h2>";
 echo"<p>".$bugDesc."</p>";
 
-$sql="SELECT * FROM comments WHERE bugID=".$_GET["id"];
-$result=mysqli_query($db,$sql);
-while ($row=mysqli_fetch_assoc($result)){
-    $commentTitle=$row['title'];
-    $commetn=$row['comment'];
-
-    echo'<h3>'.$title.'</h3>';
-    echo'<p>'.$comment.'</p>';
-}
 ?>
 <!doctype html>
 <head>
@@ -40,15 +31,8 @@ while ($row=mysqli_fetch_assoc($result)){
         <input type="date" name="date" placeholder="Date Of Bug"><br>
         <label>Date Bug Fixed</label>
         <input type="date" name="date" placeholder="Date Fixed"><br>
-        <?php
-            if (isset($_POST['SUBMIT1'])) {
-
-            }
-        if (isset($ch1=='Fixed')) {
-            $ch1='checked';
-        }
-        ?>
-        <input type="Checkbox" name="ch1" value="Fixed">
+        <label>If checked IT has fixed Bug</label>
+        <input type="Checkbox" name=".$ch1" value="Fixed">
         <?php
             print $ch1;
         ?>
