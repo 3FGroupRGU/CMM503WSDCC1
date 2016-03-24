c677f8e1129218b44e3b191f190782968be7b179
HenryRL <1300427@rgu.ac.uk>
2016-03-24 22:59:25 +0000

    247

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ require 'connect.php';?>
             $password=$_POST['pass2'];
             $phone=$_POST['telNo'];
 
-            $sql ="INSERT INTO user(username, password, email, phone)Values($username,SHA1($password),$email,$phone)";
+            $mysqli ="INSERT INTO user(username, password, email, phone)Values($username,SHA1($password),$email,$phone)";
 
             header('location: login.php');
         }
